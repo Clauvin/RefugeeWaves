c130fcca454b16025186c6794fdb899746ee5c66
bconfessor <brian_confessor@poli.ufrj.br>
2017-01-21 20:50:38 -0200

    created Time Manager Class

---
+++ UnityProject/Assets/Scripts/Managers/TimeManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour {
+
+	public static TimeManager instance;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
