25b87db1b18296e0b1393851aae817dee423f10f
bconfessor <brian_confessor@poli.ufrj.br>
2017-01-21 10:13:58 -0200

    Added Stats Manager

---
+++ UnityProject/Assets/Scripts/StatsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsManager : MonoBehaviour {
+
+
+	public static StatsManager instance;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
