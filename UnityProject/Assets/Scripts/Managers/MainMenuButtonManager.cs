2563173f166979e7492a8823d368277eed47293e
Clauvin <clauvin_almeida@hotmail.com>
2017-01-21 19:58:58 -0200

    Criado Script MainMenuButtonManager
    Para os botões do Menu Principal terem de onde ativar suas funções.

---
+++ UnityProject/Assets/Scripts/Managers/MainMenuButtonManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtonManager : MonoBehaviour {
+
+    public static void CarregaMenuPrincipal()
+    {
+        Basicas_2.CarregaCena.CarregaMenuPrincipal();
+    }
+
+    public static void CarregaGameplay()
+    {
+        Basicas_2.CarregaCena.CarregaGameplay();
+    }
+
+    public static void FechaJogo()
+    {
+        Basicas_2.CarregaCena.FechaJogo();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
