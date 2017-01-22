c0b325027aba8d41b4b4ce89d70bf5f2a226b216
bconfessor <brian_confessor@poli.ufrj.br>
2017-01-22 12:01:20 -0200

    Created Random Events and Random Event Manager
    Similar to Player Actions, Random Events have names, descriptions and
    consequences(variable that suffers consequences depend on var passed).

---
+++ UnityProject/Assets/Scripts/Managers/RandomEventManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Saves Random Events and checks to see if they happen
+public class RandomEventManager : MonoBehaviour {
+
+	public static RandomEventManager instance;
+
+	List<RandomEvent> randomEvents = new List<RandomEvent>();
+
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UnityProject/Assets/Scripts/RandomEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEvent {
+	//Random event; Has a name, a description and a consequence
+
+	string name;
+	string description;
+
+	MiscInfo.variableTypes consequenceVar;
+	float consequenceValue;
+
+
+
+
+
+
+}
