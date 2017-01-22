3f2caa1f51a30bbf840fd464d40a404883dcf5be
Clauvin <clauvin_almeida@hotmail.com>
2017-01-22 11:56:05 -0200

    Update de Immigrant Wave Launcher
    Função de instância com movimento do Refugiado funcionando. Falta o
    randomizador de posições, tempo de geração e quantidade de refugiados.

--- UnityProject/Assets/Scripts/ImmigrantWaveLauncher.cs
+++ UnityProject/Assets/Scripts/ImmigrantWaveLauncher.cs
@@ -12,15 +12,28 @@ public class ImmigrantWaveLauncher : MonoBehaviour {
     ///     d) Velocidade para chegar
     /// </summary>
 
-    void InstanciarIconeDeRefugiados()
+    public int idDoProximoIcone = 0;
+
+    public GameObject saida_refugiados_1, saida_refugiados_2;
+    public GameObject entrada_refugiados_1, entrada_refugiados_2;
+
+    void InstanciarIconeDeRefugiados(float x, float y, int quant_de_refugiados, GameObject saida, GameObject entrada, float tempo_em_segundos)
     {
         GameObject icone = Instantiate<GameObject>(Resources.Load<GameObject>("Onda De Imigrantes"));
+        icone.transform.position = new Vector3(saida.transform.position.x,
+                                               saida.transform.position.y,
+                                               saida.transform.position.z);
+        icone.GetComponent<ImmigrantWave>().numberOfImmigrants = quant_de_refugiados;
+        icone.GetComponent<MovingToTheObjective>().objetivo_final = entrada.transform.position;
+        icone.GetComponent<MovingToTheObjective>().tempo = tempo_em_segundos;
+        icone.name = "Icone de Refugiados - ID " + idDoProximoIcone;
+        idDoProximoIcone++;
     }
 
     // Use this for initialization
     void Start () {
-
-	}
+        InstanciarIconeDeRefugiados(5.0f, 2.0f, 1000, saida_refugiados_1, entrada_refugiados_1, 20.0f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
