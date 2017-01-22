d52db04663e65e8e0612c9e280c0d52e327beed8
Clauvin <clauvin_almeida@hotmail.com>
2017-01-22 14:19:17 -0200

    Refugiados Geráveis de Forma Procedural...
    ...através de InstanciarIconeDeRefugiados.

--- UnityProject/Assets/Scripts/ImmigrantWaveLauncher.cs
+++ UnityProject/Assets/Scripts/ImmigrantWaveLauncher.cs
@@ -17,7 +17,8 @@ public class ImmigrantWaveLauncher : MonoBehaviour {
     public GameObject saida_refugiados_1, saida_refugiados_2;
     public GameObject entrada_refugiados_1, entrada_refugiados_2;
 
-    void InstanciarIconeDeRefugiados(float x, float y, int quant_de_refugiados, GameObject saida, GameObject entrada, float tempo_em_segundos)
+    void InstanciarIconeDeRefugiados(int quant_de_refugiados, GameObject saida, GameObject entrada, float tempo_em_segundos,
+            float escala = 1.0f)
     {
         GameObject icone = Instantiate<GameObject>(Resources.Load<GameObject>("Onda De Imigrantes"));
         icone.transform.position = new Vector3(saida.transform.position.x,
@@ -27,12 +28,61 @@ public class ImmigrantWaveLauncher : MonoBehaviour {
         icone.GetComponent<MovingToTheObjective>().objetivo_final = entrada.transform.position;
         icone.GetComponent<MovingToTheObjective>().tempo = tempo_em_segundos;
         icone.name = "Icone de Refugiados - ID " + idDoProximoIcone;
+        if (escala > 1.0f)
+        {
+            icone.transform.localScale = new Vector3(icone.transform.localScale.x * escala,
+                                                     icone.transform.localScale.y * escala,
+                                                     1);
+        }
         idDoProximoIcone++;
+    }
+
+    void InstanciaAleatoriaInstantaneaDeOnda()
+    {
+        int quant_de_refugiados;
+        //Ano 1: de 50 a 150 refugiados por onda.
+        //Ano 2: de 150 a 200 refugiados por onda.
+        GameObject saida, entrada;
+        float tempo_em_segundos;
+        //Ano 1: de 30 a 60 segundos.
+        //Ano 2: de 15 a 30 segundos.
+        float escala = 1.0f;
+
+        //Superonda: 750 a 1000 refugiados, de 150 a 300 segundos. Sprite 3x maior.
+
+        if (TimeManager.instance.year == 1)
+        {
+            quant_de_refugiados = 50 + (int)(100 * Random.Range(0.0f, 1.0f));
+            tempo_em_segundos = 30 + 30 * Random.Range(0.0f, 1.0f);
+        }
+        else if (Random.Range(0.0f, 1.0f) < 0.05f)
+        {
+            quant_de_refugiados = 750 + (int)(250 * Random.Range(0.0f, 1.0f));
+            tempo_em_segundos = 150 + (int)(150 * Random.Range(0.0f, 1.0f));
+            escala *= 3;
+        }
+        else
+        {
+            quant_de_refugiados = 150 + (int)(50 * Random.Range(0.0f, 1.0f));
+            tempo_em_segundos = 15 + 15 * Random.Range(0.0f, 1.0f);
+        }
+
+        if (Random.Range(0.0f, 1.0f) < 0.5f)
+        {
+            saida = saida_refugiados_1; entrada = entrada_refugiados_1;
+        } else
+        {
+            saida = saida_refugiados_2; entrada = entrada_refugiados_2;
+        }
+
+        InstanciarIconeDeRefugiados(quant_de_refugiados, saida, entrada, tempo_em_segundos, escala);
     }
 
     // Use this for initialization
     void Start () {
-        InstanciarIconeDeRefugiados(5.0f, 2.0f, 1000, saida_refugiados_1, entrada_refugiados_1, 20.0f);
+        InstanciaAleatoriaInstantaneaDeOnda();
+        InstanciaAleatoriaInstantaneaDeOnda();
+        InstanciaAleatoriaInstantaneaDeOnda();
     }
 	
 	// Update is called once per frame
