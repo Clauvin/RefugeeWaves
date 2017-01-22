2c7b26a385abb4b9e1e007f62f1705a764a02bbc
Clauvin <clauvin_almeida@hotmail.com>
2017-01-22 10:28:51 -0200

    Criação de ImmigrantWaveLauncher
    ImmigrantWaveLauncher é responsável por instanciar refugirados que vão
    do ponto A ao ponto B, e chegando no ponto B, entram no país.

--- UnityProject/Assets/Scripts/ImmigrantWaveLauncher.cs
+++ UnityProject/Assets/Scripts/ImmigrantWaveLauncher.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class ImmigrantWaveLauncher : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    /// <summary>
+    /// Instancia um Ícone de Refugiados, com:
+    ///     a) Número de Imigrantes; (semi randomizado com base em valores)
+    ///     b) Ponto de instância
+    ///     c) Ponto de chegada
+    ///     d) Velocidade para chegar
+    /// </summary>
+
+    void InstanciarIconeDeRefugiados()
+    {
+        GameObject icone = Instantiate<GameObject>(Resources.Load<GameObject>("Onda De Imigrantes"));
+    }
+
+    // Use this for initialization
+    void Start () {
+
 	}
 	
 	// Update is called once per frame
