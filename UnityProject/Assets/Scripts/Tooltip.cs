589669da3ae0714500896842313cecc77abf02ae
Clauvin <clauvin_almeida@hotmail.com>
2017-01-22 04:08:38 -0200

    Add Tooltips
    Comentários nas imagens para o jogador saber do q se trata

---
+++ UnityProject/Assets/Scripts/Tooltip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tooltip : MonoBehaviour {
+    public GameObject text;
+
+	public void showPanel()
+    {
+        this.gameObject.GetComponent<Image>().enabled = true;
+        text.SetActive(true);
+    }
+    public void hiddenPanel() {
+        this.gameObject.GetComponent<Image>().enabled = false;
+        text.SetActive(false);
+    }
+    public void Start() {
+        this.gameObject.GetComponent<Image>().enabled = false;
+        text.SetActive(false);
+    }
+
+}
