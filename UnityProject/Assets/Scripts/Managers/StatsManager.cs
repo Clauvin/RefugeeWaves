02307893f22ba3b3942c680a75d574e9113271a1
Clauvin <clauvin_almeida@hotmail.com>
2017-01-22 08:55:23 -0200

    Simulação Transformada em Código
    Só falta chamar as funções a cada fim de mês, aparentemente.

--- UnityProject/Assets/Scripts/Managers/StatsManager.cs
+++ UnityProject/Assets/Scripts/Managers/StatsManager.cs
@@ -43,10 +43,92 @@ public class StatsManager : MonoBehaviour {
 	//Used when month ends, recalculates all values based on current values
 	public void calculateStatsValues()
 	{
-		//TODO: Get EXACTLY how each variable affects the other
+        //TODO: Get EXACTLY how each variable affects the other
+        //TODO: Se possível, que os imigrantes se tornando cidadãos
+        // virem ANTES desses cálculos, quando ambos ocorrerem no mesmo mês.
+
+        //Em ordem, a partir do um valor que não muda
+
+        //Se + imigrantes ilegais, opinião sobre imigrantes cai
+        //MAYBE: Reduzir a opinião pública pela porcentagem de imigrantes ilegais/população total?
+        //BYTHEWAY: número mágico de imigrantes ilegais ferrando o país por enquanto: 10000.
+        if (ImmigrantManager.instance.numberOfIllegalImmigrants > 0)
+            publicOpinionOnImmigrants -= ImmigrantManager.instance.numberOfIllegalImmigrants * 0.01f;
+
+        //Se moradia < 0, criminalidade >
+        if (ResourceManager.instance.numberOfAvailableHouses < 0)
+            criminalityRate += (ResourceManager.instance.numberOfAvailableHouses * -1 * 0.01f);
+
+        //Se social expenses < 0, criminalidade >
+        //Yes, -10000 social resources = DOOM
+        if (ResourceManager.instance.socialResources < 0)
+            criminalityRate += ResourceManager.instance.socialResources * -1 * 0.01f;
+
+        //Se border expenses < 0, criminalidade 2x >
+        //Yes, -10000 border resources = DOOM
+        if (ResourceManager.instance.borderResources < 0)
+            criminalityRate += ResourceManager.instance.borderResources * -1 * 0.01f;
+
+        //Se border officials < onda de imigrantes, criminalidade 2x >
+        //Peraí, não faz sentido. Se o cálculo sobre as ondas é feito no momento de contato, então esse
+        // criminalidade 2x precisa ser é feito na hora da colisão da onda, seja isso onde for.
+        //if (ResourceManager.instance.numberOfAvailableBorderOfficers)
+
+        //Se dinheiro < 0, criminalidade >
+        if (ResourceManager.instance.playerCurrentMoney < 0)
+            criminalityRate += ResourceManager.instance.playerCurrentMoney * -1 * 0.01f;
+
+        //Se variação do imposto +, desemprego -
+        if (ResourceManager.instance.taxVariation > 1.1)
+            unemployementRate += (ResourceManager.instance.taxVariation - 1.1) * 50f;
+
+        //Se desemprego +, variação do imposto -
+        if (ResourceManager.instance.taxVariation < 0.9)
+            unemployementRate -= (1.1 - ResourceManager.instance.taxVariation) * 50f;
+
+        //Se taxa de desemprego +, criminalidade >
+        if (unemployementRate > 0.1)
+            criminalityRate += (unemployementRate - 0.1f) * 0.1f;
+
+        //Se taxa de desemprego -, criminalidade <
+        if (unemployementRate < 0.1)
+            criminalityRate -= (0.1 - unemployementRate) * 0.1f;
+
+        //Se taxa de desemprego >, imposto p/ cidadão <
+        if (unemployementRate > 0.12)
+            ResourceManager.instance.taxVariation -= unemployementRate - 0.12;
+
+        //Se + criminalidade, Opinião Sobre Imigrantes -
+        //Yes. A 20% crimeRate means a 5% fall in publicOpinion, a 30% crimeRate means a 15% fall, and so on.
+        if (criminalityRate > 0.1)
+            publicOpinionOnImmigrants -= (criminalityRate - 0.1) * 0.5;
+
+        //Se - criminalidade, Opinião Sobre Imigrantes +
+        if (criminalityRate < 0.04)
+            publicOpinionOnImmigrants += (0.04 - criminalityRate);
+
+        //Se + criminalidade, Variação do Imposto Sobre População -
+        if (criminalityRate > 0.1)
+            ResourceManager.instance.taxVariation -= unemployementRate - 0.1;
+
+        //Se variação do imposto +, imposto p/ cidadão +
+        //Se variação do imposto -, imposto p/ cidadão -
+        //CALMA AÊ. Isso é uma consequência da equação, não é algo a ser calculado no fim do mês
+
+        //Se + VARIAÇÃO DO IMPOSTO, Opinião Sobre Imigrantes -
+        if (ResourceManager.instance.taxVariation > 1.1)
+            publicOpinionOnImmigrants -= ResourceManager.instance.taxVariation - 1.1;
+
+        //Se + opinião sobre imigrantes, Opinião Internacional +
+        if (publicOpinionOnImmigrants < 0.3)
+            internationalOpinion -= (0.3 - publicOpinionOnImmigrants) * 0.5;
+
+        //Se - opinião sobre imigrantes, Opinião Internacional -
+        if (publicOpinionOnImmigrants > 0.7)
+            internationalOpinion += (publicOpinionOnImmigrants - 0.7) * 0.5;
 
 
-	}
+    }
 
     public void UpdateFrontLegalPopulation() {
 
