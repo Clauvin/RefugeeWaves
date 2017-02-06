f4a7c130d72eeb09c355682cb01e68fc40c3952b
bconfessor <brian_confessor@poli.ufrj.br>
2017-02-06 18:37:58 -0200

    Further worked on Random event Scripts

--- UnityProject/Assets/Scripts/Managers/RandomEventManager.cs
+++ UnityProject/Assets/Scripts/Managers/RandomEventManager.cs
@@ -8,11 +8,28 @@ public class RandomEventManager : MonoBehaviour {
 	public static RandomEventManager instance;
 
 	List<RandomEvent> randomEvents = new List<RandomEvent>();
+	List<bool> randomEventsHappened = new List<bool>();//stores a bool that'll say whether the associated event already happened
 
+
+
+	//creates them all
+	public void CreateRandomEvents ()
+	{
+
+	}
+
+	//every week, this needs to be checked
+	public void CheckForRandomEvents()
+	{
+
+
+
+	}
 
 	// Use this for initialization
 	void Start () {
 		instance = this;
+		CreateRandomEvents ();
 	}
 	
 	// Update is called once per frame
--- UnityProject/Assets/Scripts/RandomEvent.cs
+++ UnityProject/Assets/Scripts/RandomEvent.cs
@@ -8,11 +8,20 @@ public class RandomEvent {
 	string name;
 	string description;
 
-	MiscInfo.variableTypes consequenceVar;
-	float consequenceValue;
+	MiscInfo.variableTypes consequenceVariable1;//Variable that suffers from this event
+	float consequenceValue1;
+	MiscInfo.variableTypes consequenceVariable2;//Possible second variable that suffers from this event
+	float consequenceValue2;
 
-
-
+	public RandomEvent(string n, string desc, MiscInfo.variableTypes var1, float consequence1, MiscInfo.variableTypes var2=MiscInfo.variableTypes.NULL, float consequence2=0.0f)
+	{
+		name = n;
+		description = desc;
+		consequenceVariable1 = var1;
+		consequenceValue1 = consequence1;
+		consequenceVariable2 = var2;
+		consequenceValue2 = consequence2;
+	}
 
 
 
