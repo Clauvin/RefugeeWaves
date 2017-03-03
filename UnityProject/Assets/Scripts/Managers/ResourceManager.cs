ed2ee0692881a32b76a2639fe4ed0fec6b14b59e
Clauvin <clauvin_almeida@hotmail.com>
2017-03-03 19:15:44 -0300

    (Fixing #14) WAAAAAY... more money?!
    1/10 = 0, so the legalPopulation and the unemploymentRate were just NOT
    adding money to the playerMonthlyBudget.
    
    With that fixed, the playerMonthlyBudget skyrocketed again, so dividing
    the effects of the legalPopulation by 200 should now place the money in
    the player's hands back in a more meaningful amount.

--- UnityProject/Assets/Scripts/Managers/ResourceManager.cs
+++ UnityProject/Assets/Scripts/Managers/ResourceManager.cs
@@ -189,7 +189,7 @@ public class ResourceManager : MonoBehaviour {
 	{
 		recalculateTax ();
         playerMonthlyBudget = realTaxPerCitizen;
-        playerMonthlyBudget *= 1 / 10 * StatsManager.instance.legalPopulation * (1 - StatsManager.instance.unemployementRate);
+        playerMonthlyBudget *= 0.005f * StatsManager.instance.legalPopulation * (1 - StatsManager.instance.unemployementRate);
         playerMonthlyBudget += BudgetBaseValue;
 	}
 
