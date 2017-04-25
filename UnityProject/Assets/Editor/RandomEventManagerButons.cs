578a116e104b17d36e50c72003f9dffca119bf74
Clauvin <clauvin_almeida@hotmail.com>
2017-04-24 23:37:30 -0300

    (#26) New Button in the RandomEventManager appears!

---
+++ UnityProject/Assets/Editor/RandomEventManagerButons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RandomEventManager))]
+public class RandomEventManagerButons : Editor {
+
+    private string an_event_happens = "An Event Happens";
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        RandomEventManager myScript = (RandomEventManager)target;
+        if (GUILayout.Button(an_event_happens))
+        {
+            myScript.AnEventHappens();
+        }
+    }
+}
