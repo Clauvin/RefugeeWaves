cfb324b5d3d58ac58e3e4f01a3cb84830eed3a93
Clauvin <clauvin_almeida@hotmail.com>
2017-05-02 22:12:24 -0300

    (#32) ActionConstructor class started
    ActionConstructor creates the PlayerActions that the player will use in
    the game and also stores the functions that will be called by the
    actions.

---
+++ UnityProject/Assets/Scripts/ActionConstructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    public static class ActionConstructor
+    {
+        public static double BuildUnnecessaryLandmarksResult()
+        {
+            return -1 * 0.1 * StatsManager.instance.unemploymentRate;
+        }
+
+        public static PlayerAction BuildUnnecessaryLandmarks()
+        {
+            return new PlayerAction(ActionsManager.instance.buttons[0],
+                "Build unnecessary landmarks",
+            "No, we don't need new bridges. But people like looking at them and" +
+            "need work, so we'll have one anyway",
+            300.0,
+            4 * ActionsManager.instance.weekLength,
+            MiscInfo.variableTypes.unemployementRate,
+            BuildUnnecessaryLandmarksResult());
+        }
+
+
+
+
+
+    }
+}
