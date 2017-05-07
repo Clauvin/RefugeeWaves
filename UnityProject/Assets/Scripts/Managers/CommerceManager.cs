94e2b69c82b51cc48bbac32f134eb9012a51005a
Clauvin <clauvin_almeida@hotmail.com>
2017-05-07 00:22:15 -0300

    (#38) Started class CommerceManager

---
+++ UnityProject/Assets/Scripts/Managers/CommerceManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class to store data regarding prices for buying/selling things
+/// Also stores the last amount bought of something, so the player don't have to input the same preferred value
+/// all the time.
+/// </summary>
+public class CommerceManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
