5f6a84bcb937f01c74945af3280b43167810f985
Clauvin <clauvin_almeida@hotmail.com>
2017-05-09 00:50:52 -0300

    (#34) #Region for ImmigrantWave

--- UnityProject/Assets/Scripts/ImmigrantWave.cs
+++ UnityProject/Assets/Scripts/ImmigrantWave.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Holds information for an immigrant wave(can be big or small)
+/// </summary>
 [System.Serializable]
 public class ImmigrantWave : MonoBehaviour {
 
 
-	//Holds information for an immigrant wave(can be big or small)
-
-	public int numberOfImmigrants;
+    
+    #region Public Variables
+    public int numberOfImmigrants;
 	public float timeOfArrival; //time it arrived in your country
 	public float timeToBecomeLegal;//time it takes for a wave to become part of the country(if they entered legally)
 	public bool isLegalWave; //true if they're legal immigrants
 
 	public bool becameCitizens; //true if they are now legal citizens
+    #endregion
 
-	public bool checkIfBecameLegal()
+    public bool checkIfBecameLegal()
 	{
 		if (isLegalWave && Time.time - timeOfArrival >= timeToBecomeLegal)
 			return true;
