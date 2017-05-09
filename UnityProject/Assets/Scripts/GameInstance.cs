dfcbee95a434c7f52f665665d4f43167f1d83336
Clauvin <clauvin_almeida@hotmail.com>
2017-05-09 00:50:36 -0300

    (#34) #Region in GameInstance

--- UnityProject/Assets/Scripts/GameInstance.cs
+++ UnityProject/Assets/Scripts/GameInstance.cs
@@ -6,58 +6,60 @@ using UnityEngine;
 public class GameInstance  {
 
 	//Class is used to create a serialized saved game; must have all relevant info about the game
-
 	public static GameInstance current;
 
 
-	//Stats variables
-	public int legalPopulation;//how many legal, tax paying ppl are living in your country atm
+
+    #region Stats Variables
+    public int legalPopulation;//how many legal, tax paying ppl are living in your country atm
 
 	public double publicOpinionOnImmigrants; //goes from 0 to 1
 	public double internationalOpinion; //goes from 0 to 1, affects your chances of getting international help
 
 	public double unemployementRate; //from 0 to 1(if 1, boy, are you screwed)
 	public double criminalityRate; //from 0 to 1(if 1, boy, are you dead)
+    #endregion
 
-
-	//Resource variables
-	public long playerCurrentMoney; //because the world revolves around it
+    //Resource variables
+    public long playerCurrentMoney; //because the world revolves around it
 	public long playerMonthlyBudget; //how much the player will earn; depends on taxPerCitizen and BudgetBaseValue
 
-	//invisible variables
-	public double baseTaxPerCitizen; //how much each citizen pays(base)
+    #region Invisible Variables
+    public double baseTaxPerCitizen; //how much each citizen pays(base)
 	public double realTaxPerCitizen; //How much they pay(with possible extra variation)
 	public double taxVariation; //i.e., inflation. Dumbass.
 	public double BudgetBaseValue;
+    #endregion
 
 
-	//Resources
-
-	public int numberOfAvailableHouses;//number of vacant houses for immigrants
+    #region Resources
+    public int numberOfAvailableHouses;//number of vacant houses for immigrants
 	public int numberOfTotalHouses; //total number of houses player has built for helping refugees
 	public int costOfHouse; 
 	public int socialResources; //number of social expenses 'units' player has to give out to refugees
 	public int costOfSocialResource;
+    #endregion
 
-	//closed borders
-	public int numberOfAvailableBorderOfficers; //number of officers not busy at the moment
+    //closed borders
+    #region Closed Borders Variables
+    public int numberOfAvailableBorderOfficers; //number of officers not busy at the moment
 	public int numberOfTotalBorderOfficers; //total number of officers player currently has employed
 	public int costOfBorderOffice;
 
 	public int borderResources; //number of border expenses 'units' player has to maintain borders running
 	public int costOfBorderResource;
+    #endregion
 
-
-	//Immigrant Waves Variables
-	public List<ImmigrantWave> legalWaves;//holds the waves of legal immigrants
+    #region Immigrant Waves Variables
+    public List<ImmigrantWave> legalWaves;//holds the waves of legal immigrants
 	public List<ImmigrantWave> illegalWaves;
 	public int numberOfLegalImmigrants;
 	public int numberOfIllegalImmigrants;
-
 	public int numberOfNaturalizedImmigrants;//how many immigrants you have helped get citizenship
+    #endregion
 
 
-	public GameInstance (int lp, double pubOp, double intOp, double unempRat, double crimiRat, double money, double mBud,
+    public GameInstance (int lp, double pubOp, double intOp, double unempRat, double crimiRat, double money, double mBud,
 		double baseTax, double realTax, double dTax, double budBase, int nAHouse, int nTotHouse, int costH, int sR,
 		int costSR, int nABO, int nTotBO, int costBO, int bRes, int costBRes, List<ImmigrantWave> lW, List<ImmigrantWave> ilW,
 		int nLIm, int nIlIm)
