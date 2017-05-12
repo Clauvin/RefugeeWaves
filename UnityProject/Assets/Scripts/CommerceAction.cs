4e253f135f827cdf017a2b8916d8e45ae87eae26
Clauvin <clauvin_almeida@hotmail.com>
2017-05-12 20:18:48 -0300

    (#38) CommerceActions variables and a constructor

--- UnityProject/Assets/Scripts/CommerceAction.cs
+++ UnityProject/Assets/Scripts/CommerceAction.cs
@@ -4,8 +4,49 @@ using UnityEngine;
 
 public class CommerceAction : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public string actionName;
+    public string actionDescription;
+
+    public GameObject assignedButton;
+
+    public delegate void consequenceFunction();
+
+    #region Consequence Variables
+    public consequenceFunction buyValueFunction;
+    public consequenceFunction sellValueFunction;
+
+    public PlayerAction doFunction;
+    #endregion
+
+    #region Cooldown Variables
+    public float actionCooldownPeriod;
+
+    public float timeLastUsed;//saves time action was last used
+    public bool isActive;//tells if action can be used or if it's cooling down
+    #endregion
+
+    #region Public Constructors
+    public CommerceAction(string actName, string actDescription, GameObject assignButton, consequenceFunction bFunction,
+        consequenceFunction sFunction, PlayerAction dFunction, float actCoolPeriod)
+    {
+        actionName = actName;
+        actionDescription = actDescription;
+        assignedButton = assignButton;
+
+        buyValueFunction = bFunction;
+        sellValueFunction = sFunction;
+        doFunction = dFunction;
+
+        actionCooldownPeriod = actCoolPeriod;
+
+        timeLastUsed = 0.0f;
+        isActive = true;
+    }
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
