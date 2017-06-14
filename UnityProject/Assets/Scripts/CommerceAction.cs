32f5c66062da5d083a291f9f84bab4b22e69c41b
Clauvin <clauvin_almeida@hotmail.com>
2017-06-13 21:32:07 -0300

    (#41) Part 2 of 3 done

--- UnityProject/Assets/Scripts/CommerceAction.cs
+++ UnityProject/Assets/Scripts/CommerceAction.cs
@@ -86,10 +86,9 @@ public class CommerceAction : MonoBehaviour {
         //instantiates a prefab with the info of the event
         GameObject newEvent = (GameObject)Instantiate(ActionsManager.instance.commerceEventPrefab);
 
-        
-
         //make it a child of the MainCanvas and adjust its scale
-        newEvent.transform.SetParent(GameObject.Find("MainCanvas").transform, false);
+        newEvent.transform.SetParent(GameObject.Find("WindowsCanvas").transform, false);
+        newEvent.name = "Commerce Window";
 
         newEvent.transform.GetComponent<CommerceEventGO>().ca = this;
 
