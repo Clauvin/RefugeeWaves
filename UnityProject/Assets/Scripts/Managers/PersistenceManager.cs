08040b36865924ac5a6885403145e82d2a3f0ef1
Clauvin <clauvin_almeida@hotmail.com>
2017-06-17 21:26:19 -0300

    (#27) Adding a static manager for saving

--- UnityProject/Assets/Scripts/Managers/PersistenceManager.cs
+++ UnityProject/Assets/Scripts/Managers/PersistenceManager.cs
@@ -6,14 +6,14 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 public static class PersistenceManager {
 
-	public static List<GameInstance> games = new List<GameInstance>();
+    public static GameInstance game;
 
 	public static void saveGame()
 	{
-		games.Add (GameInstance.current);
+        game = new GameInstance();
 		BinaryFormatter bf = new BinaryFormatter ();
 		FileStream file = File.Create (Application.persistentDataPath + "savedGames.waves");
-		bf.Serialize (file, PersistenceManager.games);
+		bf.Serialize (file, PersistenceManager.game);
 	}
 
 }
