52844b2d9ac26b0e33e86e8a67e314e05a72a359
Clauvin <clauvin_almeida@hotmail.com>
2017-06-17 21:26:03 -0300

    (#27) Adding buttons to SaveLoadManager in the editor

---
+++ UnityProject/Assets/Editor/SaveLoadManagerButtons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SaveLoadManager))]
+public class SaveLoadManagerButtons : Editor
+{
+
+    private string save_game = "Save Game";
+    private string load_game = "Load Game";
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        SaveLoadManager myScript = (SaveLoadManager)target;
+
+        if (GUILayout.Button(save_game))
+        {
+            Debug.Log(Application.persistentDataPath);
+            myScript.Save();
+        }
+
+        if (GUILayout.Button(load_game))
+        {
+
+        }
+    }
+}
