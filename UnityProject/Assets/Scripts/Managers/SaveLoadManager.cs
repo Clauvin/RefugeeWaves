b426bd523255b60ad4b6eb75b444ab6c233c0abe
Clauvin <clauvin_almeida@hotmail.com>
2017-06-17 21:25:46 -0300

    (#27) Creating SaveLoadManager

---
+++ UnityProject/Assets/Scripts/Managers/SaveLoadManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoadManager : MonoBehaviour {
+
+    public static SaveLoadManager instance;
+
+    public void Save()
+    {
+        PersistenceManager.saveGame();
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        instance = this;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
