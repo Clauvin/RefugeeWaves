4f43970730cecfcd0607651d6f61c694aee574db
Clauvin <clauvin_almeida@hotmail.com>
2017-06-20 22:50:28 -0300

    (#27) Trying to fix the load function

--- UnityProject/Assets/Scripts/GameInstance.cs
+++ UnityProject/Assets/Scripts/GameInstance.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 [System.Serializable]
 public class GameInstance  {
 
-	//Class is used to create a serialized saved game; must have all relevant info about the game
-	public static GameInstance current;
-
     public ResourceManager.ResourceSavePackage resource_save_package;
     public TimeManager.TimeSavePackage time_save_package;
 
@@ -15,13 +12,12 @@ public class GameInstance  {
     {
         resource_save_package = ResourceManager.instance.GetResourceSavePackage();
         time_save_package = TimeManager.instance.GetTimeSavePackage();
-        current = this;
     }
 
     public void PlacingSavedFilesBack()
     {
-        ResourceManager.instance.SetResourceSavePackage(current.resource_save_package);
-        TimeManager.instance.SetTimeSavePackage(current.time_save_package);
+        ResourceManager.instance.SetResourceSavePackage(resource_save_package);
+        TimeManager.instance.SetTimeSavePackage(time_save_package);
     }
 
 
