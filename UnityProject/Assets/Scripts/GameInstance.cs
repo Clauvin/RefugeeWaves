066637175d3ad6523522423a1a723a93415efd1a
Clauvin <clauvin_almeida@hotmail.com>
2017-06-22 22:22:17 -0300

    (#27) Added save/load capacity to CommerceManager

--- UnityProject/Assets/Scripts/GameInstance.cs
+++ UnityProject/Assets/Scripts/GameInstance.cs
@@ -8,12 +8,14 @@ public class GameInstance  {
     public ResourceManager.ResourceSavePackage resource_save_package;
     public TimeManager.TimeSavePackage time_save_package;
     public StatsManager.StatsSavePackage stats_save_package;
+    public CommerceManager.CommerceSavePackage commerce_save_package;
 
     public GameInstance()
     {
         resource_save_package = ResourceManager.instance.GetResourceSavePackage();
         time_save_package = TimeManager.instance.GetTimeSavePackage();
         stats_save_package = StatsManager.instance.GetStatsSavePackage();
+        commerce_save_package = CommerceManager.instance.GetCommerceSavePackage();
     }
 
     public void PlacingSavedFilesBack()
@@ -21,6 +23,7 @@ public class GameInstance  {
         ResourceManager.instance.SetResourceSavePackage(resource_save_package);
         TimeManager.instance.SetTimeSavePackage(time_save_package);
         StatsManager.instance.SetStatsSavePackage(stats_save_package);
+        CommerceManager.instance.SetCommerceSavePackage(commerce_save_package);
     }
 
 
--- UnityProject/Assets/Scripts/Managers/CommerceManager.cs
+++ UnityProject/Assets/Scripts/Managers/CommerceManager.cs
@@ -11,28 +11,124 @@ public class CommerceManager : MonoBehaviour {
 
     public static CommerceManager instance;
 
+    #region Classe Para Salvar
+    [System.Serializable]
+    public class CommerceSavePackage
+    {
+        public double _house_buy_price = 300.0f;
+        public double _house_buy_multiplier = 1.0f;
+        public double _house_sell_price = 150.0f;
+        public double _house_sell_multiplier = 1.0f;
+
+        public double _social_resource_buy_price = 100.0f;
+        public double _social_resource_buy_multiplier = 1.0f;
+        public double _social_resource_sell_price = 50.0f;
+        public double _social_resource_sell_multiplier = 1.0f;
+
+        public double _border_officer_buy_price = 125.0f;
+        public double _border_officer_buy_multiplier = 1.0f;
+        public double _border_officer_sell_price = 62.5f;
+        public double _border_officer_sell_multiplier = 1.0f;
+
+        public double _border_resource_buy_price = 75.0f;
+        public double _border_resource_buy_multiplier = 1.0f;
+        public double _border_resource_sell_price = 37.5f;
+        public double _border_resource_sell_multiplier = 1.0f;
+    }
+    #endregion
+
+    public CommerceSavePackage commerce_save_package;
+
     //CommerceActions List
     #region Buy/Sell Values
-    public double house_buy_price = 300.0f;
-    public double house_buy_multiplier = 1.0f;
-    public double house_sell_price = 150.0f;
-    public double house_sell_multiplier = 1.0f;
+    public double house_buy_price {
+        get { return commerce_save_package._house_buy_price; }
+        set { commerce_save_package._house_buy_price = value; }
+    }
 
-    public double social_resource_buy_price = 100.0f;
-    public double social_resource_buy_multiplier = 1.0f;
-    public double social_resource_sell_price = 50.0f;
-    public double social_resource_sell_multiplier = 1.0f;
+    public double house_buy_multiplier {
+        get { return commerce_save_package._house_buy_multiplier; }
+        set { commerce_save_package._house_buy_multiplier = value; }
+    }
 
-    public double border_officer_buy_price = 125.0f;
-    public double border_officer_buy_multiplier = 1.0f;
-    public double border_officer_sell_price = 62.5f;
-    public double border_officer_sell_multiplier = 1.0f;
+    public double house_sell_price {
+        get { return commerce_save_package._house_sell_price; }
+        set { commerce_save_package._house_sell_price = value; }
+    }
 
-    public double border_resource_buy_price = 75.0f;
-    public double border_resource_buy_multiplier = 1.0f;
-    public double border_resource_sell_price = 37.5f;
-    public double border_resource_sell_multiplier = 1.0f;
+    public double house_sell_multiplier {
+        get { return commerce_save_package._house_sell_multiplier; }
+        set { commerce_save_package._house_sell_multiplier = value; }
+    }
+
+    public double social_resource_buy_price {
+        get { return commerce_save_package._social_resource_buy_price; }
+        set { commerce_save_package._social_resource_buy_price = value; }
+    }
+
+    public double social_resource_buy_multiplier {
+        get { return commerce_save_package._social_resource_buy_multiplier; }
+        set { commerce_save_package._social_resource_buy_multiplier = value; }
+    }
+
+    public double social_resource_sell_price {
+        get { return commerce_save_package._social_resource_sell_price; }
+        set { commerce_save_package._social_resource_sell_price = value; }
+    }
+
+    public double social_resource_sell_multiplier {
+        get { return commerce_save_package._social_resource_sell_multiplier; }
+        set { commerce_save_package._social_resource_sell_multiplier = value; }
+    }
+
+    public double border_officer_buy_price {
+        get { return commerce_save_package._border_officer_buy_price; }
+        set { commerce_save_package._border_officer_buy_price = value; }
+    }
+
+    public double border_officer_buy_multiplier {
+        get { return commerce_save_package._border_officer_buy_multiplier; }
+        set { commerce_save_package._border_officer_buy_multiplier = value; }
+    }
+
+    public double border_officer_sell_price {
+        get { return commerce_save_package._border_officer_sell_price; }
+        set { commerce_save_package._border_officer_sell_price = value; }
+    }
+    public double border_officer_sell_multiplier {
+        get { return commerce_save_package._border_officer_sell_multiplier; }
+        set { commerce_save_package._border_officer_sell_multiplier = value; }
+    }
+
+    public double border_resource_buy_price {
+        get { return commerce_save_package._border_resource_buy_price; }
+        set { commerce_save_package._border_resource_buy_price = value; }
+    }
+
+    public double border_resource_buy_multiplier {
+        get { return commerce_save_package._border_resource_buy_multiplier; }
+        set { commerce_save_package._border_resource_buy_multiplier = value; }
+    }
+
+    public double border_resource_sell_price {
+        get { return commerce_save_package._border_resource_sell_price; }
+        set { commerce_save_package._border_resource_sell_price = value; }
+    }
+    public double border_resource_sell_multiplier {
+        get { return commerce_save_package._border_resource_sell_multiplier; }
+        set { commerce_save_package._border_resource_sell_multiplier = value; }
+    }
     #endregion
+
+    public CommerceSavePackage GetCommerceSavePackage()
+    {
+        return commerce_save_package;
+    }
+
+    public void SetCommerceSavePackage(CommerceSavePackage r)
+    {
+        commerce_save_package = r;
+    }
 
     public List<GameObject> buttons;//buttons to be assigned to each action
 
@@ -42,6 +138,7 @@ public class CommerceManager : MonoBehaviour {
     void Awake()
     {
         instance = new CommerceManager();
+        commerce_save_package = new CommerceSavePackage();
     }
 
     // Use this for initialization
