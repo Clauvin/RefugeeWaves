055604723f9eaebf652ef016859d15cb1e1ec4fb
Clauvin <clauvin_almeida@hotmail.com>
2017-06-23 21:10:09 -0300

    (#27) Update of GameInstance for saving/loading

--- UnityProject/Assets/Scripts/GameInstance.cs
+++ UnityProject/Assets/Scripts/GameInstance.cs
@@ -9,6 +9,8 @@ public class GameInstance  {
     public TimeManager.TimeSavePackage time_save_package;
     public StatsManager.StatsSavePackage stats_save_package;
     public CommerceManager.CommerceSavePackage commerce_save_package;
+    public List<ImmigrantWave.ImmigrantWaveSavePackage> list_immigrant_wave_save_package_legal;
+    public List<ImmigrantWave.ImmigrantWaveSavePackage> list_immigrant_wave_save_package_illegal;
 
     public GameInstance()
     {
@@ -16,6 +18,20 @@ public class GameInstance  {
         time_save_package = TimeManager.instance.GetTimeSavePackage();
         stats_save_package = StatsManager.instance.GetStatsSavePackage();
         commerce_save_package = CommerceManager.instance.GetCommerceSavePackage();
+        list_immigrant_wave_save_package_legal = new List<ImmigrantWave.ImmigrantWaveSavePackage>();
+        list_immigrant_wave_save_package_illegal = new List<ImmigrantWave.ImmigrantWaveSavePackage>();
+
+        List<ImmigrantWave> list_immigrant_wave = ImmigrantManager.instance.legalWaves;
+        foreach (ImmigrantWave i in list_immigrant_wave)
+        {
+            list_immigrant_wave_save_package_legal.Add(i.immigrant_wave_save_package);
+        }
+        list_immigrant_wave = ImmigrantManager.instance.illegalWaves;
+        foreach (ImmigrantWave i in list_immigrant_wave)
+        {
+            list_immigrant_wave_save_package_illegal.Add(i.immigrant_wave_save_package);
+        }
+
     }
 
     public void PlacingSavedFilesBack()
@@ -24,6 +40,20 @@ public class GameInstance  {
         TimeManager.instance.SetTimeSavePackage(time_save_package);
         StatsManager.instance.SetStatsSavePackage(stats_save_package);
         CommerceManager.instance.SetCommerceSavePackage(commerce_save_package);
+
+        ImmigrantManager.instance.legalWaves = new List<ImmigrantWave>();
+        foreach (ImmigrantWave.ImmigrantWaveSavePackage iwsp in list_immigrant_wave_save_package_legal)
+        {
+            ImmigrantWave iw = new ImmigrantWave(iwsp);
+            ImmigrantManager.instance.legalWaves.Add(iw);
+        }
+
+        ImmigrantManager.instance.illegalWaves = new List<ImmigrantWave>();
+        foreach (ImmigrantWave.ImmigrantWaveSavePackage iwsp in list_immigrant_wave_save_package_illegal)
+        {
+            ImmigrantWave iw = new ImmigrantWave(iwsp);
+            ImmigrantManager.instance.illegalWaves.Add(iw);
+        }
     }
 
 
