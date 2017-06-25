413d61d3d736128d55d3c9915749ea626ca623c2
Clauvin <clauvin_almeida@hotmail.com>
2017-06-25 12:42:29 -0300

    (#27) GameInstance now works with RandomEventManager

--- UnityProject/Assets/Scripts/GameInstance.cs
+++ UnityProject/Assets/Scripts/GameInstance.cs
@@ -10,6 +10,7 @@ public class GameInstance  {
     public StatsManager.StatsSavePackage stats_save_package;
     public CommerceManager.CommerceSavePackage commerce_save_package;
     public ImmigrantManager.ImmigrantSavePackage immigrant_save_package;
+    public RandomEventManager.RandomEventSavePackage random_event_save_package;
     public List<ImmigrantWave.ImmigrantWaveSavePackage> list_immigrant_wave_save_package_legal;
     public List<ImmigrantWave.ImmigrantWaveSavePackage> list_immigrant_wave_save_package_illegal;
 
@@ -20,6 +21,7 @@ public class GameInstance  {
         stats_save_package = StatsManager.instance.GetStatsSavePackage();
         commerce_save_package = CommerceManager.instance.GetCommerceSavePackage();
         immigrant_save_package = ImmigrantManager.instance.GetImmigrantSavePackage();
+        random_event_save_package = RandomEventManager.instance.GetRandomEventSavePackage();
         list_immigrant_wave_save_package_legal = new List<ImmigrantWave.ImmigrantWaveSavePackage>();
         list_immigrant_wave_save_package_illegal = new List<ImmigrantWave.ImmigrantWaveSavePackage>();
 
@@ -43,6 +45,7 @@ public class GameInstance  {
         StatsManager.instance.SetStatsSavePackage(stats_save_package);
         CommerceManager.instance.SetCommerceSavePackage(commerce_save_package);
         ImmigrantManager.instance.SetImmigrantSavePackage(immigrant_save_package);
+        RandomEventManager.instance.SetRandomEventSavePackage(random_event_save_package);
 
         ImmigrantManager.instance.legalWaves = new List<ImmigrantWave>();
         foreach (ImmigrantWave.ImmigrantWaveSavePackage iwsp in list_immigrant_wave_save_package_legal)
