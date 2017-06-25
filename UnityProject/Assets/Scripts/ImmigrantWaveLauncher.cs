037522261fc3643dfa07806fdd2e00dccebf7730
Clauvin <clauvin_almeida@hotmail.com>
2017-06-25 14:20:53 -0300

    (#27) ImmigrantWaveLauncher ready for save/load

--- UnityProject/Assets/Scripts/ImmigrantWaveLauncher.cs
+++ UnityProject/Assets/Scripts/ImmigrantWaveLauncher.cs
@@ -14,23 +14,81 @@ public class ImmigrantWaveLauncher : MonoBehaviour {
 
     public static ImmigrantWaveLauncher instance;
 
-    public int nextWaveId = 0;
-
     #region Spawn Points and Spawn Objectives
     public GameObject refugees_exit_1, refugees_exit_2;
     public GameObject refugees_entrance_1, refugees_entrance_2;
     #endregion
 
+    [Serializable]
+    public class ImmigrantWaveLauncherSavePackage
+    {
+        public int _nextWaveId = 0;
+
+        #region Public Variables
+        public float _present_time = 0.0f;
+        public float _last_time;
+        public float _time_for_next = 0.0f;
+        public int _def_quant_of_refugees = 50;
+        public float _def_time_in_seconds = 30.0f;
+        public float _refugees_multiplier = 1.0f; // this number multiplies the number of refugees in each new wave
+        #endregion
+
+        public bool _wave_instantiation_timer = true;
+    }
+
+    public ImmigrantWaveLauncherSavePackage immigrant_wave_launcher_save_package;
+
+    public ImmigrantWaveLauncherSavePackage GetImmigrantWaveLauncherSavePackage()
+    {
+        return immigrant_wave_launcher_save_package;
+    }
+
+    public void SetImmigrantWaveLauncherSavePackage(ImmigrantWaveLauncherSavePackage save_package)
+    {
+        immigrant_wave_launcher_save_package = save_package;
+    }
+
+    public int nextWaveId {
+        get { return immigrant_wave_launcher_save_package._nextWaveId; }
+        set { immigrant_wave_launcher_save_package._nextWaveId = value; }
+    }
+
     #region Public Variables
-    public float present_time = 0.0f;
-    public float last_time;
-    public float time_for_next = 0.0f;
-    public int def_quant_of_refugees = 50;
-    public float def_time_in_seconds = 30.0f;
-    public float refugees_multiplier = 1.0f; // this number multiplies the number of refugees in each new wave
+    public float present_time {
+        get { return immigrant_wave_launcher_save_package._present_time; }
+        set { immigrant_wave_launcher_save_package._present_time = value; }
+    }
+
+    public float last_time {
+        get { return immigrant_wave_launcher_save_package._last_time; }
+        set { immigrant_wave_launcher_save_package._last_time = value; }
+    }
+
+    public float time_for_next {
+        get { return immigrant_wave_launcher_save_package._time_for_next; }
+        set { immigrant_wave_launcher_save_package._time_for_next = value; }
+    }
+
+    public int def_quant_of_refugees {
+        get { return immigrant_wave_launcher_save_package._def_quant_of_refugees; }
+        set { immigrant_wave_launcher_save_package._def_quant_of_refugees = value; }
+    }
+
+    public float def_time_in_seconds {
+        get { return immigrant_wave_launcher_save_package._def_time_in_seconds; }
+        set { immigrant_wave_launcher_save_package._def_time_in_seconds = value; }
+    }
+
+    public float refugees_multiplier {
+        get { return immigrant_wave_launcher_save_package._refugees_multiplier; }
+        set { immigrant_wave_launcher_save_package._refugees_multiplier = value; }
+    }
     #endregion
 
-    private bool wave_instantiation_timer = true;
+    private bool wave_instantiation_timer {
+        get { return immigrant_wave_launcher_save_package._wave_instantiation_timer; }
+        set { immigrant_wave_launcher_save_package._wave_instantiation_timer = value; }
+    }
 
     #region Instantiation Timer Functions
     public bool GetWaveInstantiationTimer() { return wave_instantiation_timer; }
@@ -138,6 +196,7 @@ public class ImmigrantWaveLauncher : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        immigrant_wave_launcher_save_package = new ImmigrantWaveLauncherSavePackage();
         time_for_next = UnityEngine.Random.Range(30.0f, 60.0f);
         last_time = Time.time;
     }
