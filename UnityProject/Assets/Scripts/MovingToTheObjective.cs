073d77fac07c0c16d8d0450b9b5b7b8b3b06b766
Clauvin <clauvin_almeida@hotmail.com>
2017-06-25 16:18:39 -0300

    (#27) And... something here creashed the waves

--- UnityProject/Assets/Scripts/MovingToTheObjective.cs
+++ UnityProject/Assets/Scripts/MovingToTheObjective.cs
@@ -4,17 +4,56 @@ using UnityEngine;
 
 public class MovingToTheObjective : MonoBehaviour {
 
+    [System.Serializable]
+    public class ToTheObjectiveSavePackage
+    {
+        public Vector3 _initial_position;
+        public Vector3 _final_objective;
+        public float _time_to_reach_objective;
+        public float _passed_time;
+
+        private float _time_counted;
+    }
+
+    public ToTheObjectiveSavePackage to_the_objective_save_package;
+
+    public ToTheObjectiveSavePackage GetToTheObjectiveSavePackage()
+    {
+        return to_the_objective_save_package;
+    }
+
+    public void SetToTheObjectiveSavePackage(ToTheObjectiveSavePackage save_package)
+    {
+        to_the_objective_save_package = save_package;
+    }
+
     #region Public Variables
-    public Vector3 initial_position;
-    public Vector3 final_objective;
-    public float time_to_reach_objective;
-    public float passed_time;
+    public Vector3 initial_position {
+        get { return to_the_objective_save_package._initial_position; }
+        set { to_the_objective_save_package._initial_position = value; }
+    }
+
+    public Vector3 final_objective {
+        get { return to_the_objective_save_package._final_objective; }
+        set { to_the_objective_save_package._final_objective = value; }
+    }
+
+    public float time_to_reach_objective {
+        get { return to_the_objective_save_package._time_to_reach_objective; }
+        set { to_the_objective_save_package._time_to_reach_objective = value; }
+    }
+
+    public float passed_time {
+        get { return to_the_objective_save_package._passed_time; }
+        set { to_the_objective_save_package._passed_time = value; }
+    }
     #endregion
 
     private float time_counted;
 
 	// Use this for initialization
 	void Start () {
+        //to_the_objective_save_package = new ToTheObjectiveSavePackage();
         initial_position = new Vector2(transform.position.x, transform.position.y);
         passed_time = 0;
         time_counted = Time.time;
