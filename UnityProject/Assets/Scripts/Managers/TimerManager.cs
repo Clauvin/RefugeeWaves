e930da229b1f69b631b4045e8bd4f6a303e37059
Clauvin <clauvin_almeida@hotmail.com>
2017-07-08 18:38:48 -0300

    (#50) Created TimerManager

---
+++ UnityProject/Assets/Scripts/Managers/TimerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable()]
+public class TimerManager : MonoBehaviour {
+
+    #region Public Variables
+    public static ActionsManager instance;
+    #endregion 
+
+    // Use this for initialization
+    void Start () {
+        instance = this;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
