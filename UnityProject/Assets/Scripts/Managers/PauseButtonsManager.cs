52c98efdaa867c4ed40f7553b63f5fd5b47f647e
Clauvin <clauvin_almeida@hotmail.com>
2017-07-08 16:11:12 -0300

    (#53) New class to manage pause window buttons

---
+++ UnityProject/Assets/Scripts/Managers/PauseButtonsManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButtonsManager : MonoBehaviour {
+
+    public void LoadMainMenu()
+    {
+        Basics_3.LoadScene.LoadMainMenu();
+    }
+
+    public void LoadGame()
+    {
+        Basics_3.LoadScene.LoadMainGameplay();
+    }
+
+    public void SaveGame()
+    {
+        Basics_3.LoadScene.CloseGame();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
