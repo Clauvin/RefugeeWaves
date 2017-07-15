f7c0f0d6472cdea36591c7ecb9d566feecc2b33c
Clauvin <clauvin_almeida@hotmail.com>
2017-07-15 17:42:16 -0300

    (#58) Making save and load return boolean

--- UnityProject/Assets/Scripts/Managers/PersistenceManager.cs
+++ UnityProject/Assets/Scripts/Managers/PersistenceManager.cs
@@ -3,28 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System;
 
 public static class PersistenceManager
 {
 
     public static GameInstance game;
 
-    public static void saveGame()
+    public static bool saveGame()
     {
-        game = new GameInstance();
-        game.PreparingSaveFile();
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/savedGames.waves");
-        bf.Serialize(file, PersistenceManager.game);
-        file.Close();
+        try
+        {
+            game = new GameInstance();
+            game.PreparingSaveFile();
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Create(Application.persistentDataPath + "/savedGames.waves");
+            bf.Serialize(file, PersistenceManager.game);
+            file.Close();
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+        return true;
+
     }
 
-    public static void loadGame()
+    public static bool loadGame()
     {
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.OpenRead(Application.persistentDataPath + "/savedGames.waves");
-        game = (GameInstance)bf.Deserialize(file);
-        game.PlacingSavedFilesBack();
-        file.Close();
+        try
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.OpenRead(Application.persistentDataPath + "/savedGames.waves");
+            game = (GameInstance)bf.Deserialize(file);
+            game.PlacingSavedFilesBack();
+            file.Close();
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+        return true;
     }
 }
