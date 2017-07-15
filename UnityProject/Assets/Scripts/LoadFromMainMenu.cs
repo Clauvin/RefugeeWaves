f35892d2630eeb7ebf9a8b28d18bff1ec03c1c16
Clauvin <clauvin_almeida@hotmail.com>
2017-07-15 19:40:34 -0300

    (#58) Loading working... sort of

--- UnityProject/Assets/Scripts/LoadFromMainMenu.cs
+++ UnityProject/Assets/Scripts/LoadFromMainMenu.cs
@@ -5,6 +5,29 @@ using UnityEngine;
 public class LoadFromMainMenu : MonoBehaviour {
 
     public bool only_once = false;
+
+    public void Loading()
+    {
+        if ((only_once) && (ResourceManager.instance != null) && (TimeManager.instance != null)
+            && (TimerManager.instance != null) && (StatsManager.instance != null) && (CommerceManager.instance != null)
+            && (ImmigrantManager.instance != null) && (ImmigrantWaveLauncher.instance != null)
+            && (RandomEventManager.instance != null) && (ImmigrantManager.instance != null)
+            && (ImmigrantWaveLauncher.instance != null) && (ActionsManager.instance != null))
+        {
+            if (PersistenceManager.loadGame())
+            {
+                only_once = false;
+                DestroyObject(this.gameObject);
+            }
+            else
+            {
+                Debug.Log("Loading bugged.");
+                only_once = false;
+                DestroyObject(this.gameObject);
+            }
+        }
+    }
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,20 +35,6 @@ public class LoadFromMainMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (only_once)
-        {
-            if (PersistenceManager.loadGame())
-            {
-                only_once = false;
-                Destroy(this);
-            }
-            else
-            {
-                Debug.Log("Loading bugged.");
-                only_once = false;
-                Destroy(this);
-            }
-        }
-
+        Loading();
 	}
 }
