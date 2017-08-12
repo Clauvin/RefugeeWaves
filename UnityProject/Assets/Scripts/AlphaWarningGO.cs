b3362a1cd633cf87e81fd0d05de9b1b46cf0db1d
Clauvin <clauvin_almeida@hotmail.com>
2017-08-12 15:37:59 -0300

    (#82) Created AlphaWarningGO class, for the window OK button

---
+++ UnityProject/Assets/Scripts/AlphaWarningGO.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaWarningGO : MonoBehaviour
+{
+
+    public void PressedOKEventButton()
+    {
+        Basics_3.LoadScene.LoadMainMenu();
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
