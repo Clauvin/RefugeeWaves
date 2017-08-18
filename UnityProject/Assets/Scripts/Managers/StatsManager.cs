8d1f5c885fb6fe93e81092cd3dce3fd8188d1645
Clauvin <clauvin_almeida@hotmail.com>
2017-08-18 00:01:01 -0300

    (#88) Raise in crime by illegals divided by 100

--- UnityProject/Assets/Scripts/Managers/StatsManager.cs
+++ UnityProject/Assets/Scripts/Managers/StatsManager.cs
@@ -101,7 +101,7 @@ public class StatsManager : MonoBehaviour {
         if (ImmigrantManager.instance.numberOfIllegalImmigrants > 0)
         {
             publicOpinionOnImmigrants -= ImmigrantManager.instance.numberOfIllegalImmigrants * 0.005f;
-            criminalityRate += ImmigrantManager.instance.numberOfIllegalImmigrants * 0.0005f;
+            criminalityRate += ImmigrantManager.instance.numberOfIllegalImmigrants * 0.000005f;
         }
 
         //If border expenses < 0, crime 2x >
