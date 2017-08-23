07a0107999214a63c11e46670370aabd94bdf368
Clauvin <clauvin_almeida@hotmail.com>
2017-08-22 22:10:18 -0300

    (#99) Created class TimeSkip McGyverism

---
+++ UnityProject/Assets/Scripts/TimeSkipMcGyverism.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSkipMcGyverism : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        TimeManager.PauseOrUnpause();
+        IfPausedShowsElseDont.TogglePause();
+        PauseButtonsManager.EraseSaveLoad();
+
+    }
+	
+}
