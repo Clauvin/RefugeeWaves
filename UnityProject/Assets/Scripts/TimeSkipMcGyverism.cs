94ba52e53e41aff8eb81665c6d90681cf0c5add3
Clauvin <clauvin_almeida@hotmail.com>
2017-08-22 22:30:39 -0300

    (#99) McGyverist solution working

--- UnityProject/Assets/Scripts/TimeSkipMcGyverism.cs
+++ UnityProject/Assets/Scripts/TimeSkipMcGyverism.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class TimeSkipMcGyverism : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject pause_button_manager;
 
-        TimeManager.PauseOrUnpause();
-        IfPausedShowsElseDont.TogglePause();
-        PauseButtonsManager.EraseSaveLoad();
+    private bool mcgyverism_1 = true;
+
+	// Use this for initialization
+	void Update () {
+
+        if ((TimeManager.instance != null) && (mcgyverism_1)) {
+            TimeManager.instance.PauseOrUnpause();
+            GetComponent<IfPausedShowsElseDont>().TogglePause();
+            pause_button_manager.GetComponent<PauseButtonsManager>().EraseSaveLoadText();
+            mcgyverism_1 = false;
+        }
+
+
 
     }
 	
