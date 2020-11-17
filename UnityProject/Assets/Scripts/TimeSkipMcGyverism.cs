92b0a5d6ec106606dc4f6a82e9fac337f93de1dd
Cláuvin <Clauvin@users.noreply.github.com>
2020-11-17 15:30:32 -0300

    (#99) Fixed the "starting at week 2" bug

--- UnityProject/Assets/Scripts/TimeSkipMcGyverism.cs
+++ UnityProject/Assets/Scripts/TimeSkipMcGyverism.cs
@@ -16,9 +16,14 @@ public class TimeSkipMcGyverism : MonoBehaviour {
             GetComponent<IfPausedShowsElseDont>().TogglePause();
             pause_button_manager.GetComponent<PauseButtonsManager>().EraseSaveLoadText();
             mcgyverism_1 = false;
+
+            if (TimeManager.instance.year == 1 && TimeManager.instance.month == 1 && TimeManager.instance.week == 1)
+            {
+                TimeManager.instance.timeElapsed = 0.0f;
+                TimeManager.instance.timeLastPaused = 0.0f;
+                TimeManager.instance.timeLastUnpaused = 0.0f;
+            }
         }
-
-
 
     }
 	
