3d72feceaed7f6ad19f41d054f0ee43771449b26
Cláuvin <Clauvin@users.noreply.github.com>
2020-12-13 21:29:03 -0300

    (#64) Created IconGoesAway script

---
+++ UnityProject/Assets/Scripts/IconGoesAway.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconGoesAway : MonoBehaviour
+{
+
+    bool it_disappeared_yet = false;
+    public int month_to_disappear;
+    public SpriteRenderer icon_to_disappear_yet;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!it_disappeared_yet)
+        {
+            if (month_to_disappear <= TimeManager.instance.month)
+            {
+                icon_to_disappear_yet.enabled = false;
+                it_disappeared_yet = true;
+            }
+        }
+    }
+}
