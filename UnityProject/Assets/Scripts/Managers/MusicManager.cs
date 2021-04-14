2158fda5e7c18dff2cb0fe3de79e5291a4d27154
Cláuvin <Clauvin@users.noreply.github.com>
2021-04-14 00:00:50 -0300

    (#115) Ok, now everything is set up

--- UnityProject/Assets/Scripts/Managers/MusicManager.cs
+++ UnityProject/Assets/Scripts/Managers/MusicManager.cs
@@ -52,6 +52,40 @@ public class MusicManager : MonoBehaviour {
         audio_source.Stop();
     }
 
+    private void ReadjustLobbyMusicVolume()
+    {
+        ActOnLobbyMusicByMonthAndWeek(TimeManager.instance.month, TimeManager.instance.week);
+    }
+
+    private void ActOnLobbyMusicByMonthAndWeek(int month, int week)
+    {
+        switch (month)
+        {
+            case 4:
+                if (week == 2) MusicManager.instance.lobby_music.volume = 1.0f;
+                if (week == 3) MusicManager.instance.lobby_music.volume = 0.9f;
+                if (week == 4) MusicManager.instance.lobby_music.volume = 0.8f;
+                Debug.Log("What");
+                break;
+            case 5:
+                if (week == 1) MusicManager.instance.lobby_music.volume = 0.7f;
+                if (week == 2) MusicManager.instance.lobby_music.volume = 0.6f;
+                if (week == 3) MusicManager.instance.lobby_music.volume = 0.5f;
+                if (week == 4) MusicManager.instance.lobby_music.volume = 0.4f;
+                Debug.Log("What");
+                break;
+            case 6:
+                if (week == 1) MusicManager.instance.lobby_music.volume = 0.3f;
+                if (week == 2) MusicManager.instance.lobby_music.volume = 0.2f;
+                if (week == 3) MusicManager.instance.lobby_music.volume = 0.1f;
+                if (week == 4) MusicManager.instance.lobby_music.volume = 0.0f;
+                Debug.Log("What");
+                break;
+            default:
+                break;
+        }
+    }
+
     void Awake()
     {
         instance = this;
@@ -72,7 +106,7 @@ public class MusicManager : MonoBehaviour {
 
         if (crisis_music.isPlaying)
         {
-            ReadjustVolume();
+            ReadjustLobbyMusicVolume();
         }
 
 	}
