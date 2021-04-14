404d2fe9d8906b478245c350ca15612de120e3c1
Cláuvin <Clauvin@users.noreply.github.com>
2021-04-14 19:21:02 -0300

    (#115) Created OpenCCAttributionSite script

---
+++ UnityProject/Assets/Scripts/OpenCCAttributionSite.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCCAttributionSite : MonoBehaviour {
+
+	public void OpenCCASite()
+    {
+        Application.OpenURL("https://creativecommons.org/licenses/by/3.0/");
+    }
+}
