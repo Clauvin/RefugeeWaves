0f82032db3eb5e9e0866aad34ca5fd2b955da5b7
Cláuvin <Clauvin@users.noreply.github.com>
2021-04-21 13:12:58 -0300

    (#116) Created FeedbackManager

---
+++ UnityProject/Assets/Scripts/Managers/FeedbackManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackManager : MonoBehaviour {
+
+	[System.Serializable]
+
+	public class FeedbacksGiven
+    {
+		public bool public_works_feedback;
+		public bool edicts_for_start_ups_feedback;
+		public bool public_security_feedback;
+		public bool campaign_non_refugee_immigrants_feedback;
+		public bool create_ondestao_feedback;
+		public bool create_pro_ondestao_feedback;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
