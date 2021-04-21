558eed59931a9de9ca7e2f0ffa359adf347d5feb
Cláuvin <Clauvin@users.noreply.github.com>
2021-04-21 14:37:05 -0300

    (#116) Created HelpEvent class

---
+++ UnityProject/Assets/Scripts/HelpEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpEvent : MonoBehaviour {
+
+	public string name;
+	public string description;
+
+	public HelpEvent(string n, string desc)
+	{
+		name = n;
+		description = desc;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
